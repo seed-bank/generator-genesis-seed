7e4f780d6318f0bd9a59cb27d98c28f60006276c
RhysAnexture <rhys@anexture.com>
2018-01-16 15:59:58 -0500

    Adding the generation of the subgenerators init package. All subgenerators will have an associative init in order to generate the genesis.config file.

--- generators/submodule/templates/generators/submodule-init/index.js
+++ generators/submodule/templates/generators/submodule-init/index.js
@@ -9,14 +9,12 @@ module.exports = class extends Generator {
     genesis.bang(this, function (yeoman) {
       return new Promise(function(resolve, reject) {
         prompt.start();
-        console.log(chalk.red("Provide a name for the generator."));
+        console.log(chalk.red("Provide a name for the submodule."));
         console.log("alpha numberic only");
-        console.log("use "+ chalk.red( "'-'") + " for spaces.");
-        console.log("I will prefix the name with "+ chalk.red( "'generator-'") + " to adhere to yeoman naming conventions.")
-        prompt.get(['name', 'author'], function (err, result) {
+        console.log("No spaces");
+        prompt.get(['name'], function (err, result) {
           yeoman.data = {};
           yeoman.data.name      = "generator-" + result.name;
-          yeoman.data.author    = result.author;
           resolve();
         });
       });
